5687cb06ee39580f077de7d91e891e89e174bfc1
fkgrkyr <fkgrkyr@gmail.com>
2025-07-24 18:09:17 +0900

    fix: 구글 소셜로그인 해제

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
-const clientId =
-  '883406660074-egus3t3tbp8tbepfstht7bo10akobrvq.apps.googleusercontent.com';
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </GoogleOAuthProvider>
   </StrictMode>
 );
