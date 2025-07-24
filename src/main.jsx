89bfcbfdbb48795fe071aa9c148731470e3a021c
chaeapark <yunapark2002@gmail.com>
2025-07-24 17:57:03 +0900

    feat: 맵 페이지 UI 구현

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
+const clientId =
+  '883406660074-egus3t3tbp8tbepfstht7bo10akobrvq.apps.googleusercontent.com';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={clientId}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>
 );
