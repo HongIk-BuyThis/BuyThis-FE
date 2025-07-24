0cdbc59a9e718aac3a1be420fafefa0a6d8f684a
chaeapark <yunapark2002@gmail.com>
2025-07-24 14:49:03 +0900

    fix: navbar 크기 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import Logo from '../assets/KPOP_CAFE_HUNTERS.svg';
 import LanguageIcon from '../assets/LanguageFilter.svg';
-import LoginIcon from '../assets/LogIn.svg';
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -12,23 +11,24 @@ const Navbar = () => {
       <img
         src={Logo}
         alt="KPOP CAFE HUNTERS"
-        className="h-6 cursor-pointer"
+        className="h-10 cursor-pointer" // ← h-6 → h-10
         onClick={() => navigate('/')}
       />
 
       {/* 오른쪽 버튼 영역 */}
       <div className="flex items-center gap-6">
         {/* 언어 필터 버튼 */}
-        <button className="flex items-center gap-2 bg-main-bright px-3 py-1.5 rounded-full text-gray-900 text-sm font-medium">
-          <img src={LanguageIcon} alt="언어 아이콘" className="w-4 h-4" />
+        <button className="flex items-center gap-2 px-6 py-2 rounded-[10px] bg-[#DFF8F5] text-black text-sm font-normal">
+          <img src={LanguageIcon} alt="언어 아이콘" className="w-5 h-5" />
+          언어 필터링
         </button>
 
         {/* 로그인 */}
         <div
           onClick={() => navigate('/login')}
-          className="flex items-center gap-1 text-gray-100 text-sm font-pretendard cursor-pointer hover:underline"
+          className="flex items-center gap-1 text-[#000] text-sm font-normal cursor-pointer"
         >
-          <img src={LoginIcon} alt="로그인 아이콘" className="w-4 h-4" />
+          로그인
         </div>
       </div>
     </header>
