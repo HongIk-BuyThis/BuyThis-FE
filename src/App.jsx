e5e05dedf5a378fe11ccf631025aa83c91373970
chaeapark <yunapark2002@gmail.com>
2025-07-24 12:50:15 +0900

    environment: 개발환경 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-// 공통 컴포넌트
-import Navbar from "./components/Navbar";
-
-// 페이지 컴포넌트
-import MainPage from "./pages/MainPage";
-import LoginPage from "./pages/LoginPage";
-import MapPage from "./pages/MapPage";
-import CelebrityPage from "./pages/CelebrityPage";
-import CafePage from "./pages/CafePage";
-import TourTips from "./pages/TourTips";
-import NotFound from "./pages/NotFound"; // 404 페이지
+import Navbar from './components/Navbar';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import MapPage from './pages/MapPage';
+import CelebrityPage from './pages/CelebrityPage';
+import CafePage from './pages/CafePage';
+import TourTips from './pages/TourTips';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
@@ -25,11 +21,10 @@ const App = () => {
         <Route path="/celebrity" element={<CelebrityPage />} />
         <Route path="/cafe" element={<CafePage />} />
         <Route path="/tips" element={<TourTips />} />
-
         {/* 없는 주소 접속 시 404 */}
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
