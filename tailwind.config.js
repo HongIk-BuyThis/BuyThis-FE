c407fa6c61faf19f37688f77c9026fdbdb45dff9
chaeapark <yunapark2002@gmail.com>
2025-07-24 13:19:42 +0900

    feat: 디자인 시스템 적용 - 타이포그래피 및 컬러 시스템 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
+      fontSize: {
+        h1: ['36pt', { lineHeight: '1', letterSpacing: '0' }],
+        h2: ['32pt', { lineHeight: '1', letterSpacing: '0' }],
+        h3: ['24pt', { lineHeight: '1', letterSpacing: '0' }],
+        body1: ['16pt', { lineHeight: '1', letterSpacing: '0' }],
+        body2: ['20pt', { lineHeight: '1', letterSpacing: '0' }],
+      },
+      fontWeight: {
+        regular: '400',
+        bold: '700',
+      },
+      colors: {
+        gray: {
+          10: '#F5F5F5',
+          20: '#E5E5E5',
+          30: '#D4D4D4',
+          40: '#AFAFAF',
+          50: '#999999',
+          60: '#7A7A7A',
+          70: '#5E5E5E',
+          80: '#4B4B4B',
+          90: '#2F2F2F',
+          100: '#1A1A1A',
+        },
+      },
+    },
   },
   plugins: [],
 };
